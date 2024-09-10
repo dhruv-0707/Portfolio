658e377557178edb10e336e110abff233e55c4b1
Dhruv <dhruvnautiyal0707@gmail.com>
2024-09-11 00:22:35 +0530

    final first look

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const more_proj = document.querySelector('.more-projects-btn');
+const phone_c = document.querySelector('.phone');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
 });
@@ -9,7 +10,14 @@ function scrollToSection(sectionId) {
     behavior: 'smooth'
   });
 }
-more_proj.addEventListener('click',() =>{
+more_proj.addEventListener('click', () => {
   window.location.href = "projects.html";
 });
-
+phone_c.addEventListener('click', () => {
+  navigator.clipboard.writeText("+91 7037052148").then(() => {
+    // Show an alert dialog
+    alert("Phone No Copied");
+  }).catch(err => {
+    console.error("Some error occured", err);
+  });
+});

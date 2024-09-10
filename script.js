1c808f319e84c2c5a94cccd65d4d17891521ac5c
Dhruv <dhruvnautiyal0707@gmail.com>
2024-09-11 01:12:55 +0530

    some new functionality added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const more_proj = document.querySelector('.more-projects-btn');
 const phone_c = document.querySelector('.phone');
+const text = "+91 7037052148";
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
 });
@@ -12,12 +13,16 @@ function scrollToSection(sectionId) {
 }
 more_proj.addEventListener('click', () => {
   window.location.href = "projects.html";
+  alert("hello");
 });
 phone_c.addEventListener('click', () => {
-  navigator.clipboard.writeText("+91 7037052148").then(() => {
+  
+});
+function copytext(){
+  navigator.clipboard.writeText(text).then(() => {
     // Show an alert dialog
     alert("Phone No Copied");
   }).catch(err => {
     console.error("Some error occured", err);
   });
-});
+}
